c7a84f226e17943f4c321753f011f8276ca220df
user <user@DESKTOP-JM511V4>
2023-07-11 09:25:17 -0500

    carga inicial

---
+++ src/modelo/Cliente.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author user
+ */
+public class Cliente {
+    String codCliente;
+    String ruc;
+    String razonSocial;
+    String direccion;
+}
---
+++ src/modelo/colaborador.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author user
+ */
+public class colaborador {
+    String codColab;
+    String dni;
+    String nombre;
+    String apellido;
+    String cargo;
+}
