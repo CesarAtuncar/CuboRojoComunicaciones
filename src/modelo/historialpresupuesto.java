30edc01102f2b2490620210d06faae86a451a451
user <user@DESKTOP-JM511V4>
2023-07-12 03:58:47 -0500

    modificaciones tabla de clases

---
+++ src/modelo/historialpresupuesto.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author user
+ */
+public class historialpresupuesto {
+    String actividad;
+    int fecha;
+    String nombre;
+    String razonSocial;
+    String titulo;
+    double total;
+}
---
+++ src/modelo/user.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author user
+ */
+public class user {
+    String apellido;
+    String nombre;
+    String password;
+    String userName;
+}
