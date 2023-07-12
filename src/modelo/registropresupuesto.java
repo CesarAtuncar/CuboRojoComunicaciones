2002c3ee272e80ae83bb92e87532ec5171f1cad9
user <user@DESKTOP-JM511V4>
2023-07-12 03:57:42 -0500

    modificaciones tabla de clases

---
+++ src/modelo/registropresupuesto.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author user
+ */
+public class registropresupuesto {
+    String actividad;
+    int cantidad;
+    String descripcion;
+    double feeAgencia;
+    String nombre;
+    double PrecioTotal;
+    double PrecioUnitario;
+    String razonSocial;
+    String Status;
+    double subTotal;
+    String titulo;
+    double total;
+            
+}
