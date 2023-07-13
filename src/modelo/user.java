49164becd35c3745ea35e2425c00e56dd4ce219c
user <user@DESKTOP-JM511V4>
2023-07-13 10:53:10 -0500

    modificaciones tabla de clases

--- src/modelo/user.java
+++ src/modelo/user.java
@@ -9,8 +9,52 @@ package modelo;
  * @author user
  */
 public class user {
-    String apellido;
-    String nombre;
-    String password;
-    String userName;
+    private String apellido;
+    private String nombre;
+    private String password;
+    private String userName;
+
+    public user() {
+    }
+
+    public user(String apellido, String nombre, String password, String userName) {
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.password = password;
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
 }
