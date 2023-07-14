fe07b815a71109b6df6d42975d2c64c1ee32f999
user <user@DESKTOP-JM511V4>
2023-07-14 11:23:50 -0500

    agregar clase User y modificacion UserDao

---
+++ src/modelo/User.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author user
+ */
+public class User {
+    private String UserName;
+    private String Password;
+
+    public User() {
+    }
+
+    public User(String UserName, String Password) {
+        this.UserName = UserName;
+        this.Password = Password;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String UserName) {
+        this.UserName = UserName;
+    }
+    
+    
+    
+}
